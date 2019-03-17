b2f1ac60064f426f15d4943f8359aa913fd1746a
nickchahley <nickchahley@gmail.com>
2019-03-17 17:34:30 -0400

    ResNet adapted for greyscale images

---
+++ resnet_mnist.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+import time
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Local imports
+import util
+from MnistResNet import MnistResNet
+
+
+def main():
+    args = parse_args()
+
+    # Seed for repeatability
+    torch.manual_seed(args.seed)
+
+
+    start_ts = time.time()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Init model and move to gpu, if possible
+    model = MnistResNet().to(device)
+
+    # Get the DataLoaders for train and validation (test) sets
+    train_params = {'batch_size': args.batch_size, 'shuffle': True}
+    test_params = {'batch_size': args.test_batch_size, 'shuffle': False}
+    train_loader, val_loader = util.data_loaders(test_params, train_params)
+
+    # your loss function, cross entropy works well for multi-class problems
+    loss_function = nn.CrossEntropyLoss() 
+
+    # optimizer, I've used Adadelta, as it works well without any magic numbers
+    optimizer = optim.Adadelta(model.parameters())
+
+    losses = []
+    batches = len(train_loader)
+    val_batches = len(val_loader)
+
+    # loop for every epoch (training + evaluation)
+    epochs = args.epochs
+    for epoch in range(epochs):
+        total_loss = 0
+
+        # progress bar (works in Jupyter notebook too!)
+        progress = tqdm(enumerate(train_loader), desc="Loss: ", total=batches)
+
+        # ----------------- TRAINING  -------------------- 
+        # set model to training
+        model.train()
+        
+        for i, data in progress:
+            X, y = data[0].to(device), data[1].to(device)
+            
+            # training step for single batch
+            model.zero_grad()
+            outputs = model(X)
+            loss = loss_function(outputs, y)
+            loss.backward()
+            optimizer.step()
+
+            # getting training quality data
+            current_loss = loss.item()
+            total_loss += current_loss
+
+            # updating progress bar
+            progress.set_description("Loss: {:.4f}".format(total_loss/(i+1)))
+            
+        # releasing unceseccary memory in GPU
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+        
+        # ----------------- VALIDATION  ----------------- 
+        val_losses = 0
+        precision, recall, f1, accuracy = [], [], [], []
+        
+        # set model to evaluating (testing)
+        model.eval()
+        with torch.no_grad():
+            for i, data in enumerate(val_loader):
+                X, y = data[0].to(device), data[1].to(device)
+
+                outputs = model(X) # this get's the prediction from the network
+
+                val_losses += loss_function(outputs, y)
+
+                predicted_classes = torch.max(outputs, 1)[1] # get class from network's prediction
+                
+                # calculate P/R/F1/A metrics for batch
+                for acc, metric in zip((precision, recall, f1, accuracy), 
+                                    (precision_score, recall_score, f1_score, accuracy_score)):
+                    acc.append(
+                        util.calculate_metric(metric, y.cpu(), predicted_classes.cpu())
+                    )
+            
+        print(f"Epoch {epoch+1}/{epochs}, training loss: {total_loss/batches}, validation loss: {val_losses/val_batches}")
+        util.print_scores(precision, recall, f1, accuracy, val_batches)
+        losses.append(total_loss/batches) # for plotting learning curve
+    print(f"Training time: {time.time()-start_ts}s")
+
+    if args.save_model:
+        torch.save(model.state_dict(), args.save_name)
+
+    if args.plot:
+        x_ax = range(1, epochs+1)
+        plt.xlabel('Epochs')
+        plt.ylabel('Losses')
+        plt.plot(x_ax, losses)
+        plt.xticks(np.arange(1, x_ax[-1], 1))
+        plt.show()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='ResNet for single channel ims')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+
+    parser.add_argument('--test-batch-size', type=int, default=500, metavar='N',
+                        help='input batch size for testing (default: 500)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    parser.add_argument('--save-name', type=str, default='models/resnet_mnist.pt',
+                        help='Filename to save model to (default: models/resnet_mnist.pt')
+    parser.add_argument('--plot', action='store_true', default=False,
+                        help='Plot learning curve')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    main()
---
+++ util.py
@@ -0,0 +1,108 @@
+# Utility functions
+import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import inspect
+
+class CustomDataset(Dataset):
+    """ torch Dataset generator (to feed to a DataLoader) for our MNIST style
+    data
+    X : np.ndarray, images of shape (N, 64, 64)
+    Y : a df of the train_labels.csv, with columns [Id, Category] eg output of
+        pd.read_csv('data/input/train_labels.csv')
+    """
+    def __init__(self, X, labels, transform = transforms.ToTensor()):
+        self.X = X
+        self.labels = labels
+        self.ID = np.array(self.labels['Id'])
+        self.Y = np.array(self.labels['Category'])
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        """ When the sample corresponding to a given index is called, the
+        generator executes the __getitem__ method to generate it.
+        """
+        x = self.X[idx]
+        y = self.Y[idx]
+        ID = self.ID[idx]
+
+        if self.transform:
+            x = self.transform(x)
+
+        # but what about id and label?
+        x.requires_grad_(True)
+        return x, y
+    
+    def tensor_x(self):
+        return self.transform(self.X)
+    
+    def tensor_y(self):
+        return self.transform(self.Y)
+
+def load_data(train_porportion=0.8, split_train=True, include_ids=False):
+    """ Split the training data in training/valid sets
+    include_ids: 
+        true: Ytest/train are returned as dataframes with columns [Id,
+        Category]
+        false: Ytest/train are returned as np arrays containing just Category
+    """
+
+    train_images = pd.read_pickle('data/input/train_images.pkl')
+    train_labels = pd.read_csv('data/input/train_labels.csv')
+
+    if split_train:
+        if not include_ids:
+            train_labels = np.array(train_labels['Category'])
+
+        N = len(train_images)
+        last_train_idx = int(N*train_porportion)
+        Y = np.squeeze(train_labels[:last_train_idx])
+        testY = np.squeeze(train_labels[last_train_idx:])
+        X = train_images[:last_train_idx]
+        testX = train_images[last_train_idx:]
+        return X, Y, testX, testY
+
+    else:
+        return train_images, train_labels
+
+def data_loaders(train_params, test_params, transfer_learning=False):
+    X, Y, Xtest, Ytest = load_data(include_ids=True)
+
+    # Use full training set to calculate metrics for normalizations
+    X_full = np.vstack((X, Xtest))
+
+    if transfer_learning:
+        # Repeat the greyscale channel 3x to make an rgb im
+        Transformer = transforms.Compose([
+            transforms.Resize((224, 224)), 
+            transforms.ToTensor(),
+            transforms.Normalize((X_full.mean()/255,), (X_full.std()/255,)),
+            transforms.Lambda(lambda x: x.repeat(3, 1, 1))
+        ])
+
+    else:
+        Transformer = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((X_full.mean()/255,), (X_full.std()/255,))
+        ])
+
+    train_loader = DataLoader(CustomDataset(X, Y, Transformer), **train_params)
+    test_loader = DataLoader(CustomDataset(Xtest, Ytest, Transformer), **test_params)
+
+    return train_loader, test_loader
+
+def calculate_metric(metric_fn, true_y, pred_y):
+    # multi class problems need to have averaging method
+    if "average" in inspect.getfullargspec(metric_fn).args:
+        return metric_fn(true_y, pred_y, average="macro")
+    else:
+        return metric_fn(true_y, pred_y)
+    
+def print_scores(p, r, f1, a, batch_size):
+    # just an utility printing function
+    for name, scores in zip(("precision", "recall", "F1", "accuracy"), (p, r, f1, a)):
+        print(f"\t{name.rjust(14, ' ')}: {sum(scores)/batch_size:.4f}")
