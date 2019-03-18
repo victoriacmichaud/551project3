e355dbfff7434057b34d1758d4371f72c9acbba7
nickchahley <nickchahley@gmail.com>
2019-03-18 11:09:25 -0400

    Add greyscale resnet50, 101, 152

--- MnistResNet.py
+++ MnistResNet.py
@@ -1,5 +1,5 @@
 import torch
-from torchvision.models.resnet import ResNet, BasicBlock
+from torchvision.models.resnet import ResNet, BasicBlock, Bottleneck
 
 class MnistResNet(ResNet):
     """ ResNet adapted for single (from 3) channel images
@@ -16,3 +16,51 @@ class MnistResNet(ResNet):
     def forward(self, x):
         return torch.softmax(
             super(MnistResNet, self).forward(x), dim=-1)
+
+class MnistResNet50(ResNet):
+    """ ResNet adapted for single (from 3) channel images
+    
+    https://zablo.net/blog/post/using-resnet-for-mnist-in-pytorch-tutorial
+    """
+    def __init__(self):
+        super(MnistResNet50, self).__init__(Bottleneck, [3, 4, 6, 3], num_classes=10)
+        self.conv1 = torch.nn.Conv2d(1, 64, 
+            kernel_size=(7, 7), 
+            stride=(2, 2), 
+            padding=(3, 3), bias=False)
+        
+    def forward(self, x):
+        return torch.softmax(
+            super(MnistResNet50, self).forward(x), dim=-1)
+
+class MnistResNet101(ResNet):
+    """ ResNet adapted for single (from 3) channel images
+    
+    https://zablo.net/blog/post/using-resnet-for-mnist-in-pytorch-tutorial
+    """
+    def __init__(self):
+        super(MnistResNet101, self).__init__(Bottleneck, [3, 4, 23, 3], num_classes=10)
+        self.conv1 = torch.nn.Conv2d(1, 64, 
+            kernel_size=(7, 7), 
+            stride=(2, 2), 
+            padding=(3, 3), bias=False)
+        
+    def forward(self, x):
+        return torch.softmax(
+            super(MnistResNet101, self).forward(x), dim=-1)
+
+class MnistResNet152(ResNet):
+    """ ResNet adapted for single (from 3) channel images
+    
+    https://zablo.net/blog/post/using-resnet-for-mnist-in-pytorch-tutorial
+    """
+    def __init__(self):
+        super(MnistResNet152, self).__init__(Bottleneck, [3, 8, 36, 3], num_classes=10)
+        self.conv1 = torch.nn.Conv2d(1, 64, 
+            kernel_size=(7, 7), 
+            stride=(2, 2), 
+            padding=(3, 3), bias=False)
+        
+    def forward(self, x):
+        return torch.softmax(
+            super(MnistResNet152, self).forward(x), dim=-1)
