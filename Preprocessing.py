fb962eef2b59a7a1f80f1a425301cad81ab92304
nickchahley <nickchahley@gmail.com>
2019-03-18 16:08:27 -0400

    Squash local pulls.

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,19 +4,18 @@ import pandas as pd
 import pickle
 import os
 import cv2
+import math
 
 train_labels = pd.read_csv('./train_labels.csv')
 train_images = pd.read_pickle('./train_images.pkl')
 test_images = pd.read_pickle('test_images.pkl')
 
-#print(train_images.shape)
-#print(test_images.shape)
 
-# plt.title('Label: {}'.format(train_labels.iloc[img_idx]['Category']))
-#plt.imshow(train_images[10])
-#plt.show()
+# plt.title('Label: {}'.format(train_labels.iloc[16]['Category']))
+# plt.imshow(train_images[16])
+# plt.show()
 
-"""Preprocessing using OpenCV"""
+""" Preprocessing using OpenCV : Choose between largest rectangle, ellipse, or contour """
 
 def preprocessTrainMinRect():
 	for i, img in enumerate(train_images):
@@ -34,7 +33,7 @@ def preprocessTrainMinRect():
 		area = 0
 		largestctr = ctrs[0]
 		for ctr in ctrs:
-			temparea = cv2.minAreaRect(ctr)[1][0]*cv2.minAreaRect(ctr)[1][1]
+			temparea = cv2.minAreaRect(ctr)[1][0]*cv2.minAreaRect(ctr)[1][1] #length x width
 			if (temparea>area):
 				largestctr = ctr
 				area = temparea
@@ -47,54 +46,178 @@ def preprocessTrainMinRect():
 		mask = np.zeros(img.shape, np.uint8)
 		cv2.fillPoly(mask,poly,255) #creating a mask over the irrelevant number contours
 
-		newimg = cv2.bitwise_and(img, mask) #black and white image containing only the largest number
+		newimg = cv2.bitwise_and(thresh, mask) #black and white image containing only the largest number
 		xtrain.append(newimg)
 
 def preprocessTestMinRect():
 	for i, img in enumerate(test_images):
 		img = test_images[i].astype('uint8')
 
-		#Threshold the image for values of 0 or 1
 		ret, thresh = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
 
-		#Get contours of the binary image
 		contoured, ctrs, heir = cv2.findContours(thresh,1,cv2.CHAIN_APPROX_SIMPLE)
 
-		#Get the area of the largest rectangle and contour corresponding to it
 		area = 0
 		largestctr = ctrs[0]
 		for ctr in ctrs:
-			temparea = cv2.minAreaRect(ctr)[1][0]*cv2.minAreaRect(ctr)[1][1] #length x width
+			temparea = cv2.minAreaRect(ctr)[1][0]*cv2.minAreaRect(ctr)[1][1]
 			if (temparea>area):
 				largestctr = ctr
 				area = temparea
 
-		#Make a picture of the image with only one (largest) number
 		rect = cv2.minAreaRect(largestctr)
-		box = cv2.boxPoints(rect) #get the four corners
+		box = cv2.boxPoints(rect)
 		box = np.int0(box)
 		poly = np.array([box], dtype=np.int32)
 		mask = np.zeros(img.shape, np.uint8)
-		cv2.fillPoly(mask,poly,255) #creating a mask over the irrelevant number contours, replace with black
+		cv2.fillPoly(mask,poly,255)
 
-		newimg = cv2.bitwise_and(img, mask) #black and white image containing only the largest number
+		newimg = cv2.bitwise_and(thresh, mask)
 		xtest.append(newimg)
+
+def preprocessTrainMinEllipse():
+	for i, img in enumerate(train_images):
+		img = train_images[i].astype('uint8')
+
+		ret, thresh = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
+
+		contoured, ctrs, heir = cv2.findContours(thresh,1,cv2.CHAIN_APPROX_SIMPLE)
+
+		# Get contour of largest ellipse size
+		area = 0
+		largestctr = ctrs[0]
+		for ctr in ctrs:
+			if len(ctr) >= 5:
+				(x,y), (MA, ma), angle = cv2.fitEllipse(ctr)
+				temparea = (math.pi)*MA*ma
+				if(temparea>area):
+					largestctr = ctr
+					area = temparea
+			
+
+		ellipse = cv2.fitEllipse(largestctr)
+		mask = np.zeros(img.shape, np.uint8)
+		poly = cv2.ellipse(mask, ellipse, (255,255,255),-1)
+
+		newimg = cv2.bitwise_and(thresh, thresh, mask=poly)
+		xtrain.append(newimg)
+
+def preprocessTestMinEllipse():
+	for i, img in enumerate(test_images):
+		img = test_images[i].astype('uint8')
+
+		ret, thresh = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
+
+		contoured, ctrs, heir = cv2.findContours(thresh,1,cv2.CHAIN_APPROX_SIMPLE)
+
+		area = 0
+		largestctr = ctrs[0]
+		for ctr in ctrs:
+			if len(ctr) >= 5: #fitellipse fn requires min 5 contours
+				(x,y), (MA, ma), angle = cv2.fitEllipse(ctr)
+				temparea = (math.pi)*MA*ma
+				if(temparea>area):
+					largestctr = ctr
+					area = temparea
+			
+
+		ellipse = cv2.fitEllipse(largestctr)
+		mask = np.zeros(img.shape, np.uint8)
+		poly = cv2.ellipse(mask, ellipse, (255,255,255),-1)
+
+		newimg = cv2.bitwise_and(thresh, thresh, mask=poly)
+		xtest.append(newimg)
+
+def preprocessTrainNumPixels():
+	for i, img in enumerate(train_images):
+		img = train_images[i].astype('uint8')
+
+		ret, thresh = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
+
+		contoured, ctrs, heir = cv2.findContours(thresh,1,cv2.CHAIN_APPROX_SIMPLE)
+
+		#Get contour containing largest number of pixels (area)
+		area = 0
+		largestctr = ctrs[0]
+		for ctr in ctrs:
+			temparea = cv2.contourArea(ctr)
+			if temparea>area:
+				largestctr = ctr
+				area = temparea
+
+		mask = np.zeros(img.shape, np.uint8)
+		cv2.drawContours(mask, [largestctr], -1, 255, -1)
+
+		newimg = cv2.bitwise_and(thresh, thresh, mask=mask)
+		xtrain.append(newimg)
+
+def preprocessTestNumPixels():
+	for i, img in enumerate(test_images):
+		img = test_images[i].astype('uint8')
+
+		ret, thresh = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
+
+		contoured, ctrs, heir = cv2.findContours(thresh,1,cv2.CHAIN_APPROX_SIMPLE)
+
+		#Get contour containing largest number of pixels (area)
+		area = 0
+		largestctr = ctrs[0]
+		for ctr in ctrs:
+			temparea = cv2.contourArea(ctr)
+			if temparea>area:
+				largestctr = ctr
+				area = temparea
+
+		mask = np.zeros(img.shape, np.uint8)
+		cv2.drawContours(mask, [largestctr], -1, 255, -1)
+
+		newimg = cv2.bitwise_and(thresh, thresh, mask=mask)
+		xtest.append(newimg)
+
 
 
 xtrain = []
 xtest = []
-preprocessTrainMinRect()
-preprocessTestMinRect()
-
-#Example
-# plt.imshow(xtrain[16])
-# plt.show()
 
 def save_processed(filename, dataset):
 	with open (filename, 'wb') as f:
 		pickle.dump(dataset, f)
 
-#save_processed("train_rect_1.pkl", xtrain)
-#save_processed("test_rect_1.pkl", xtest)
+""" Largest Rotated Rectangle """
+
+# preprocessTrainMinRect()
+# preprocessTestMinRect()
+
+# save_processed("train_rect_1.pkl", xtrain)
+# save_processed("test_rect_1.pkl", xtest)
+
+""" Largest Rotated Ellipse """
+
+# preprocessTrainMinEllipse()
+# preprocessTestMinEllipse()
+
+# save_processed("train_ellipse_1.pkl", xtrain)
+# save_processed("test_ellipse_1.pkl", xtest)
+
+""" Greatest Area Contour """
+
+# preprocessTrainNumPixels()
+# preprocessTestNumPixels()
+
+# save_processed("train_pix_1.pkl", xtrain)
+# save_processed("test_pix_1.pkl", xtest)
+
+#Example
+plt.imshow(train_images[12])
+plt.show()
+
+plt.imshow(xtrain[12])
+plt.show()
+
+# plt.imshow(test_images[16])
+# plt.show()
+
+# plt.imshow(xtest[16])
+# plt.show()
 
 
--- SVM.py
+++ SVM.py
@@ -10,16 +10,37 @@ import time
 
 train_labels = pd.read_csv('./train_labels.csv')
 train_images = pd.read_pickle('./train_rect_1.pkl')
-test_images = pd.read_pickle('test_rect_1.pkl')
+test_images = pd.read_pickle('./test_rect_1.pkl')
 
-X, Y, testX, testY = [], [], [], []
+# X, Y, testX, testY = [], [], [], []
 
-Y = np.squeeze(train_labels[:80000])
-testY = np.squeeze(train_labels[80000:])
-X = train_images[:80000]
-testX = train_images[80000:]
+# Y = np.squeeze(train_labels[:80000])
+# testY = np.squeeze(train_labels[80000:])
+# X = train_images[:80000]
+# testX = train_images[80000:]
 
-svm = LinearSVC()
+
+labels_formatted = train_labels.as_matrix(columns=train_labels.columns[1:])
+
+X, testX, Y, testY = train_test_split(train_images, labels_formatted, test_size = 0.2)
+#print("Array shapes: ")
+#print(np.shape(X), np.shape(testX), np.shape(Y), np.shape(testY))
+
+
+nsamples, nX, nY = np.shape(X)
+X = np.reshape(X, (nsamples, nX*nY))
+Y = np.reshape(Y, (np.shape(Y)[0], ))
+
+#print("Reshaped: ", np.shape(X), np.shape(Y))
+
+# Implement SVM
+svm = LinearSVC(verbose=3,max_iter=10000)
 svm.fit(X, Y) #train the SVM
 
+predictY = svm.predict(testX)
+
+# Model accuracy
+print("Score: ")
 svm.score(testX, testY)
+print("Accuracy: ")
+metrics.accuracy_score(testY, predictY)
